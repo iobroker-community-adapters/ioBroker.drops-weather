f7faa68bb35fb7f6e11083447a5bc3f7a13a81cb
Martin M. <mcm57@gmx.at>
2025-03-19 11:03:32 +0100

    Update npm-postinstall.js

--- npm-postinstall.js
+++ npm-postinstall.js
@@ -11,19 +11,20 @@ console.log('NPM POSTINSTALL script started');
 const arch = os.arch();
 const platform = os.platform();
 
-console.log(`installation running on ${platform} / ${arch}\n`);
+console.log(`installation running on ${platform} / ${arch}`);
+console.log('');
 
 if (!additionalPackages[arch]) {
-    console.log(`NO additonal installation required`);
+    console.log('NO additonal installation required');
     process.exit(0);
 }
 
-console.log(`starting installation of additional packages, please stand by...\n`);
+console.log('starting installation of additional packages, please stand by...');
 
 const { spawnSync } = require('node:child_process');
 const packages = additionalPackages[arch];
 for (const pkg of packages){
     console.log(`installing ${pkg}...\n`);
     const res = spawnSync(`sudo apt install ${pkg}`);
-    console.log(`${res.toString()}`);
+    console.log(`${JSON.stringify(res}`);
 }
