3c551c4f5951bb89dc474e76d1e8d97b4d209052
Martin M. <mcm57@gmx.at>
2025-03-19 09:22:04 +0100

    Update npm-postinstall.js

--- npm-postinstall.js
+++ npm-postinstall.js
@@ -1,5 +1,15 @@
 /**
  * npm post installation script
  */
+const os = require('node:os');
 
 console.log('npm-postinstall script started');
+const arch = os.arch();
+const platform = os.platform();
+
+console.log(`installation running on ${platform} / ${arch}`);
+if (arch !== 'arm'){
+    console.log(`NO additonal installation required`);
+    process.exit(0);
+}
+console.log(`starting installation of additional packages, please stand by...`);
