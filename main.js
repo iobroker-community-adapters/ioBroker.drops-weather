3744686242418367db045dc37f3307571752ea40
arteck <6681528+arteck@users.noreply.github.com>
2025-03-20 19:03:08 +0100

    corr

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ class DropsWeather extends utils.Adapter {
 
         this.log.debug(`creating new page ...`);
         try {
-            page = await browser.newPage();
+            const page = await browser.newPage();
 
             await page.setUserAgent(
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
